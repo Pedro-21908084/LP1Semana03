6b230ba20068d4aba84949c9011e51f8c026ee95
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-09 17:46:44 +0000

    Finished Exercise 1

--- PercorreString/Program.cs
+++ PercorreString/Program.cs
@@ -6,7 +6,32 @@ namespace PercorreString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //declaration of variables
+            string input, character;
+            char charToOmit;
+
+            //read input
+            Console.Write("String: ");
+            input = Console.ReadLine();
+            Console.Write("Carácter: ");
+            character = Console.ReadLine();
+            while(character.Length > 1)
+            {
+                Console.WriteLine("Error: to many characters.");
+                Console.Write("Carácter: ");
+                character = Console.ReadLine();
+            }
+            charToOmit = char.Parse(character);
+
+            //run string printing every character expect specified
+            Console.Write("Será impresso: ");
+            foreach(char c in input)
+            {
+                if(c != charToOmit)
+                    Console.Write(c);
+            }
+            Console.WriteLine();
+            
         }
     }
 }
