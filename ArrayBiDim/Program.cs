c8df885592cfb2aaf8b35638e94b1e031228337e
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-09 17:57:25 +0000

    Exercise2: read input

--- ArrayBiDim/Program.cs
+++ ArrayBiDim/Program.cs
@@ -6,7 +6,19 @@ namespace ArrayBiDim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaration of variables
+            float [,] array;
+            string input;
+
+            //Read input
+            Console.Write("Size (Ex: zXy): ");
+            input = Console.ReadLine();
+            string [] inputSplit= input.Split("X");
+            Console.WriteLine($"x={inputSplit[0]} / y={inputSplit[1]}");
+            //Resize array according to input
+            //ask for values in each position of array
+            //print means of each line
+            //print the total of all the means
         }
     }
 }
