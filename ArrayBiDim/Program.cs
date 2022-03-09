44b88a5bea33299f2c72ad1722ec83e08f4aab3b
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-09 18:04:52 +0000

    Exercise2: Resize array

--- ArrayBiDim/Program.cs
+++ ArrayBiDim/Program.cs
@@ -7,15 +7,26 @@ namespace ArrayBiDim
         static void Main(string[] args)
         {
             //Declaration of variables
-            float [,] array;
+            float[,] array;
             string input;
 
             //Read input
             Console.Write("Size (Ex: zXy): ");
             input = Console.ReadLine();
-            string [] inputSplit= input.Split("X");
-            Console.WriteLine($"x={inputSplit[0]} / y={inputSplit[1]}");
+            string[] inSplt = input.Split("X");
+            Console.WriteLine($"x={inSplt[0]} / y={inSplt[1]}");
+
             //Resize array according to input
+            array = new float[int.Parse(inSplt[0]), int.Parse(inSplt[1])];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine();
+            }
+
             //ask for values in each position of array
             //print means of each line
             //print the total of all the means
